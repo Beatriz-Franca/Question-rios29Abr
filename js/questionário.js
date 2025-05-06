f254c5906f593721936221987dad9a8d29d30492
beaa <gomesfrancabeatriz@gmail.com>
2025-05-06 19:23:48 -0300

    Add files via upload

---
+++ js/questionário.js
@@ -0,0 +1,193 @@
+// fazendo sem a funciton pois não estava dando certo 😁
+ 
+//BLOCO 1 ⚠
+ 
+// questão 1 - Cumprimento personalizado
+//  let nome = prompt("Olá, como se chama??"); // prompt que pede o nome da pessoa
+//  alert("Olá, " + nome); // alert que grava o nome da pessoa e em uma mensagem, mostra a mensagem personalizada
+ 
+// questão 2 - calculadora de idade simples
+// var dataNascimento = prompt("Digite sua data de nascimento: "); // prompt que pede a data de nacsimento
+// const dataNascimento = parseInt; // converte o texto do prompt em string
+// const dataAtual = new Date().getFullYear(); // mantém a dataAtual como int
+// alert(dataAtual - dataNascimento); // exibe a mensagem de texto com a idade da pessoa
+ 
+//questão 3 - preço total da compra
+// var precoItem1 = parseFloat(prompt ("Digite o preço do item 1: ")); //pede ao usuário o valor do item 1
+// var precoItem2 = parseFloat(prompt("Digite o preço do item 2: ")); // pede ao usuário o valor do item 2
+// var precoTotal =  precoItem1 +  precoItem2  //equação que calcula os dois valores
+// alert ("O preço total é: R$ "+ precoTotal); //equação que calcula o resultado
+ 
+//questão 4 - calculadora de troco
+//  var precoPago = parseFloat(prompt ("Digite o preço pago pelo(s) produto(s): ")); //pede ao usuário o valor pago pela mercadoria
+//  var precoTotal = parseFloat(prompt("Digite o valor total da compra: ")); // pede ao usuário o valor total da compra
+//  var valorTroco =  precoPago - precoTotal  //equação que calcula os dois valores
+//  alert ("O seu troco é de: R$ "+ valorTroco); //equação que calcula o resultado
+ 
+//questão 5 - multiplicador de pontos
+// var quantMoedas = parseInt(prompt("Digite quantas moedas você pegou: ")); // pede o usuário o número de moedas que ele possui
+// pontTotal = quantMoedas * 3  // (moeda = 3) calcula a pontuação
+// alert("Sua pontuação total é de: " + pontTotal);
+ 
+//questão 6 - divisor de despezas
+// var valorJantar = parseFloat(prompt("Digite o valor total do jantar: ")); //pede o valor gasto no jantar
+// contaDiv = valorJantar / 2; // como são duas pessoas dividindo a conta, vamos dividiro valor por 2
+// alert("Cada um vai pagar: R$ " + contaDiv); //exibe o resultado da operação
+ 
+//questão 7 - verificador de tipo de ingresso
+// var assento = parseInt(prompt("Qual o seu assento? ")); //pede o assento da pessoa
+// if (assento = (>= 10)) {
+//     alert("O tipo do seu assento é o básico");
+// } if (assento = <=20) {
+//    alert("O seu assento é o V.I.P");
+//     }
+// else {
+//    alert(" O seu assento não existe");
+// }
+
+ 
+//questão 8 - luz acesa ou apagada?
+// var statusLuz = parseInt(prompt("A luz está...  ")); //pede os status da luz
+// if (statusLuz = 'Acesa') { // função if que dependendo da ação mostra um resultado diferente
+//     luzAcesa = true; // se o resultado der 'Acesa' ela vai dizer que está acesa
+//     alert("A luz está acesa");
+   
+// } else {
+//     statusLuz = 'Apagada'; // se a luz der 'Apagada' ela vai dizer que está apagada
+//     luzAcesa = false;
+//     alert(" A luz está apagada");
+// };
+ 
+//questão 9 - nome completo do cliente
+// var nome = prompt("Olá, qual seu nome??"); // prompt que pede o nome da pessoa
+// var sobreomenome = prompt("Qual seu sobrenome??"); // prompt que pede o sobrenome da pessoa
+//  alert("Olá, "+ (nome +" " + sobreomenome)); // alert que grava o nome da pessoa e soma o nome com o sobrenome em uma mensagem, mostra a mensagem personalizada
+ 
+ //questão 10 - doces restantes
+//  var quantBalas = parseInt(prompt("Quantas balas tinham no pote?")); // pede a quantidade de balas que tinham no pote
+//  var balasComidas = parseInt(prompt("Quantas balas foram comidas?")); // pede a quantidade de balas que foram comidas
+//  alert("Existem "+ (quantBalas - balasComidas) + " no pote."); // calcula a quantidade de balas que restaram no pote e mostra essa quantidade em um alert
+  
+// BLOCO 2 ⚠
+
+  //questão 1 - entrada no cinema
+//   var idade = parseInt(prompt("Qual a sua idade?"));
+//   if ( idade >= 18) {
+//     alert("Bom filme!");
+//   } else {
+//     alert("Esse filme não é pra você");
+//   }
+
+  //questão 2 - saldo da conta
+// var saldo = parseInt(prompt("Qual seu saldo atual?")); //pede o saldo atual 
+// if (saldo >=0) { // função if else que analisa o saldo 
+//  alert("Seu saldo é positivo!"); // caso seja positivo, ele retorna positivo
+// } else {
+//   alert("Seu saldo é negativo!"); //caso seja negativo, ele retorna negativo
+//} 
+
+  //questão 3 - número da sorte par
+// var sorteio = parseInt(prompt("Qual número foi sorteado?")); //pede o número sorteado
+// if (sorteio % 2 === 0 ) { // função que analisa se o número é par ou ímpar ( o comando % divide os números para confirmar se o número é par ou ímpar)
+//   alert("Você ganhou! Parabéns") //exibe a mensagem de vitória quando o número é par
+// } else { 
+//   alert("Tente novamente...") //caso sejaa ímpar, exibe mensagem negativa
+// }
+  //questão 4 - semáfaro
+// var semaforo = prompt("Qual a cor do semáforo?");
+// if( semaforo === 'Vermelho'){
+//   alert("Pare 🛑");
+// } else if (semaforo === 'Amarelo') {
+//   alert("Preste atenção ⚠");
+// } else  if (semaforo === 'Verde') {
+//   alert("Prossiga ✔");
+// } else {
+//   alert("Inválido, digite outra.")
+// }
+
+  //questão 5 - verificação de senha simples
+// var senha = prompt("Para continuar, digite sua senha:");
+// if (senha == 'senha1234') {
+//   alert("Acesso concedido")
+// } else {
+//   alert("Acesso negado, tente novamente")
+// }
+  // BLOCO 3 ⚠
+
+  //questão 1 - contador de passos
+
+  //questão 2 - lançamento de foguete
+
+  //questão 3 - somar notas da turma
+
+  //questão 4 - listar números pares
+
+  //questão 5 - tabuada do 7  
+
+  // BLOCO 4 ⚠
+
+  //questão 1 - mensagem personalizada
+
+  //questão 2 - área do quadrado
+
+  //questão 3 - converter metros para centímetros
+
+  //questão 4 - calcular o triplo
+
+  //questão 5 - exibir alerta
+
+  // BLOCO 5 ⚠
+
+  //questão 1 - lista de compras
+
+  //questão 2 - primeiro item da lista
+
+  //questão 3 - último item da lista
+
+  //questão 4 - adicionar tarefa
+
+  //questão 5 - contando itens no carrinho
+
+  //questão 6 - somar números de um sorteio
+
+  //questão 7 - encontrar a maior pontuação
+
+  //questão 8 - mostrar convidados
+
+  //questão 9 - item na lista de desejos
+
+  //questão 10 - criar sequência numérica
+
+  // BLOCO 6 ⚠
+
+  //questão 1 - descrever um pet
+
+  //questão 2 - pegar nome do pet
+
+  //questão 3 - pegar idade do pet
+
+  //questão 4 - atualizar idade do pet
+
+  //questão 5 - descrever um livro
+
+  //qquestão 6 - mostrar informações do livro
+
+  //questão 7 - adicionar gênero do livro
+
+  // BLOCO 7 ⚠
+
+  //questão 1 - lista de contatos
+
+  //questão 2 - buscar contato por nome
+
+  //questão 3 - listar nomes dos contatos
+
+  //questão 4 - calcular média de preço
+
+  //questão 5 - verificar tamanho mínimo da mensagem
+
+  //questão 6 - contar letras 'A'
+
+  //questão 7 - repetir palavra
+
+  //questão 8 - juntar palavras de arr
